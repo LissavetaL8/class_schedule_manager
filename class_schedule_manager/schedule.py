2a9a137c051499a1decbfa3ba29b6686d149cfaf
LissavetaL8 <elizavetalopatinskaa@gmail.com>
2026-01-13 16:12:07 +0300

    Update schedule.py

--- class_schedule_manager/schedule.py
+++ class_schedule_manager/schedule.py
@@ -6,10 +6,16 @@ class Schedule:
     """Класс для хранения и обработки списка занятий."""
 
     def __init__(self):
-        self._events = []
+        """ принимает созданное расписание занятий
+        retern: none """
+        self._events = [] #список в котором будет храниться расписание занятий
 
     def add_event(self, event):
-        """Добавляет событие, проверяя на конфликты."""
+        
+        """event: объект, который нужно добавить в список
+        event.dt: Используется для проверки занятости времени
+        self._events: Список, в который добавляется новое событие"""
+        
         if self.check_conflict(event.dt):
             print(f"Ошибка: Конфликт расписания на {event.dt}")
             return False
@@ -18,26 +24,37 @@ class Schedule:
         return True
 
     def remove_event(self, subject, dt):
-        """Удаляет занятие по названию и времени."""
+        """Удаляет занятие по названию и времени
+        принимает созданное расписание занятий, название предмета, который нужно удалить
+        retern: none"""
         self._events = [e for e in self._events if not (e.subject == subject and e.dt == dt)]
 
     def check_conflict(self, dt):
-        """Проверяет, занято ли указанное время (упрощенно: окно в 1.5 часа)."""
+        """Проверяет, занято ли указанное время
+        принимает расписание и время события
+        retern: bool"""
+        
         for e in self._events:
             if abs(e.dt - dt) < timedelta(hours=1, minutes=30):
-                return True
-        return False
+                return True #если время занято другой дисциплной, в указанном интервале
+        return False #если время свободно
 
     def get_for_day(self, date_obj):
-        """Возвращает события на конкретный день."""
+        """Возвращает события на конкретный день
+        принимает рассписание и дату на которую нужно получить список пар
+        retern: расписание пар на заданный день"""
+        
         return [e for e in self._events if e.dt.date() == date_obj.date()]
 
     def show_all(self):
-        """Печать всего расписания."""
+        """Печать всего расписания
+        принимает созданное расписание 
+        retern: полное расписание на весь период"""
         for e in self._events:
             print(e)
 
 if __name__ == '__main__':
     ...
     
+
 
